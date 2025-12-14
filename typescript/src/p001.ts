4c781fbb08d218998ad51c35f89d514589f4b935
tat-u <uetatsu1005@gmail.com>
2025-12-15 00:16:50 +0900

    Refactor p001.ts

--- typescript/src/p001.ts
+++ typescript/src/p001.ts
@@ -1,19 +1,22 @@
-const NoM = (b: number, k: number) => {
-  return Math.floor((b - 1) / k);
-};
-
-const Sn = (a1: number, an: number, n: number) => {
-  return ((a1 + an) * n) / 2;
-};
-
-const BM = (n: number, k: number) => {
-  return n - 1 - ((n - 1) % k);
-};
-
 const MAX = 1000;
 
-const A = Sn(3, BM(MAX, 3), NoM(MAX, 3));
-const B = Sn(5, BM(MAX, 5), NoM(MAX, 5));
-const C = Sn(15, BM(MAX, 15), NoM(MAX, 15));
+/** Number of multiples of k in [1, m) */
+const NoM = (k: number, m: number = MAX) => {
+  return Math.floor((m - 1) / k);
+};
+
+/** Sum of an arithmetic series */
+const Sn = (a1: number, an: number, n: number) => {
+  return ((a1 + an) / 2) * n;
+};
+
+/** Biggest multiple of k below m */
+const BM = (k: number, m: number = MAX) => {
+  return m - 1 - ((m - 1) % k);
+};
+
+const A = Sn(3, BM(3), NoM(3));
+const B = Sn(5, BM(5), NoM(5));
+const C = Sn(15, BM(15), NoM(15));
 
 console.log(A + B - C);
